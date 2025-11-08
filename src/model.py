b82731c6d1afce959a8e4a16d8f05dd806f458b9
Ahmad Allabadi <allabada@mcmaster.ca>
2025-11-08 12:35:10 -0500

    fix: Minor fix in model

--- src/model.py
+++ src/model.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 
 class CNN(nn.Module):
     def __init__(self, num_classes = 2):
-        super().__init__
+        super().__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, padding = 1) # First convolution layer
-        self.pool = nn.pool = nn.MaxPool2d(2,2) # Pool layer
+        self.pool = nn.MaxPool2d(2,2) # Pool layer
         self.conv2 = nn.Conv2d(16, 32, 3, padding = 1) # Second convolution layer
         self.fc1 = nn.Linear(32 * 56 * 56, num_classes)
     
